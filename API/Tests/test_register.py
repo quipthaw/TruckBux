b8b772e9380f434b86fa21be52e6031a3cade54a
Nick Rabon <nwrabon@gmail.com>
2022-10-02 14:19:57 -0400

    added updateprof endpoint and created test

--- API/Tests/test_register.py
+++ API/Tests/test_register.py
@@ -5,7 +5,7 @@ import json
 api_host = '127.0.0.1:5000'
 
 try:
-    result = requests.post(f'http://{api_host}/register', json={'user': 'hashuser', 'pass': 'HashThisPlease$', 'email': 'nrabon@gmail.com', 'fname': 'Nick', 'lname': 'Rabon'})
+    result = requests.post(f'http://{api_host}/register', json={'user': 'regtest62', 'pass': 'NewPassword$', 'email': 'test@gmail.com', 'fname': 'test', 'lname': 'test'})
     print(result.json())
 except:
-    print('insert failed')
+    print('insert failed' + result.json())
