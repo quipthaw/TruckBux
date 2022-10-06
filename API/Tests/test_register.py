920a95aacc339803ed93f1788a5ca0ceb17e62d9
Nick Rabon <nwrabon@gmail.com>
2022-10-06 09:25:47 -0400

    Added acctType to register fields

--- API/Tests/test_register.py
+++ API/Tests/test_register.py
@@ -5,7 +5,8 @@ import json
 api_host = '127.0.0.1:5000'
 
 try:
-    result = requests.post(f'http://{api_host}/register', json={'user': 'regtest62', 'pass': 'NewPassword$', 'email': 'test@gmail.com', 'fname': 'test', 'lname': 'test'})
+    result = requests.post(f'http://{api_host}/register', json={'user': 'testdrive', 'pass': 'NewPassword$', 'email': 'test@gmail.com',
+     'fname': 'test', 'lname': 'test'})
     print(result.json())
 except:
     print('insert failed' + result.json())
