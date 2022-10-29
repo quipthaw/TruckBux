0b7e08126d1d1b2f9faa1155021bed96db379d48
nathan-frye <77402987+nathan-frye@users.noreply.github.com>
2022-10-29 19:28:04 -0400

    cart calculations fixed

--- team27-website-4910/src/components/Catalog/MyCart.js
+++ team27-website-4910/src/components/Catalog/MyCart.js
@@ -1,17 +1,33 @@
-import { Stack } from "@mui/material";
+import { Stack, Card, CardContent, Typography } from "@mui/material";
 import React from 'react';
 import { MyCartRow } from "./MyCartRow";
 
 export const MyCart = (props) => {
     const { cart, setCart } = props;
 
+    const getTotal = () => {
+        let total = 0;
+        cart.forEach((item) => {
+            total = total + Number(item.quantity * item.price);
+        });
+
+        const totalString = `Total: ${total.toFixed(2)}`
+
+        return totalString;
+    };
+
     return (
-        <Stack direction="column" spacing={2}>
+        <Stack direction="column">
             {cart.map((item) => {
                 return (
-                    <MyCartRow key={item.itemId} cart={cart} item={item} setCart={setCart}/>
+                        <MyCartRow key={item.itemId} cart={cart} item={item} setCart={setCart}/>
                 );
             })}
+            <Card sx={{ backgroundColor: "lightgreen" }}>
+                <CardContent sx={{ position: "relative", left: "15%", right: "10%", width: "75%" }}>
+                    <Typography align="right" sx={{ fontWeight: "800"}}>{getTotal()}</Typography>
+                </CardContent>
+            </Card>
         </Stack>
     );
 };
